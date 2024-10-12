5da2afaf119fa3540d71fbd45ede53ae4137ee92
Seth Morton <smphotography39@gmail.com>
2024-10-12 01:55:12 -0400

    added basic reddit api service implementation

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,14 @@
+import { RedditApiService } from "../../../service/RedditApiService";
 export const dynamic = "force-dynamic"; // defaults to auto
 export const runtime = "edge";
-
-export async function POST(request: Request) {
-  return new Response("Hello World");
+const USER_AGENT = "HackHarvard";
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
+export async function GET(request: Request) {
+  if (!AUTH_TOKEN) {
+    return new Response("No auth token found", { status: 401 });
+  }
+  const REDDIT_SERVICE = new RedditApiService(AUTH_TOKEN, USER_AGENT);
+  return new Response(
+    JSON.stringify(await REDDIT_SERVICE.getSubredditPostsData("sibo"))
+  );
 }
